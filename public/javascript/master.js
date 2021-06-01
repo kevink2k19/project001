9168cbc45685e7034ea9397d43ee2da32b8aabfd
kevink2k19 <kay.cube.18@gmail.com>
2021-06-01 16:04:26 +0630

    2021 initial version

---
+++ public/javascript/master.js
@@ -0,0 +1,17 @@
+$('#casinoClick').hover(function(){
+  $('.casinoIMG').attr("src","../img/icons/casinoG.png");
+},function(){
+$('.casinoIMG').attr("src","../img/icons/casinoBW.png");
+});
+
+$('#soccerClick').hover(function(){
+  $('.soccerIMG').attr("src","../img/icons/soccerG.png");
+},function(){
+  $('.soccerIMG').attr("src","../img/icons/soccerBW.png");
+});
+
+$('#sbClick').hover(function(){
+  $('.sbIMG').attr("src","../img/r-sideBarIMG/sb365Color.png");
+},function(){
+  $('.sbIMG').attr("src","../img/r-sideBarIMG/sb365Black.png");
+});
